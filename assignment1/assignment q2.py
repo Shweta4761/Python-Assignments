e06333d857547bd239c1b76786a0e1c943db8011
Shweta4761 <shwetasharan216@gmail.com>
2024-07-25 23:53:26 +0530

    assignment questions

---
+++ assignment1/assignment q2.py
@@ -0,0 +1,5 @@
+name="shweta sharan"
+age=20
+fav_hobby="Reading Books"
+
+print("name is ",name,"age is",age,"favourite hobby is",fav_hobby)
---
+++ assignment1/assignment q4.py
@@ -0,0 +1,8 @@
+number=int(input(print("Enter your number")))
+ 
+if number == 0 :
+    print("Number is zero")
+elif number>0:
+    print("Number is positive")
+elif number<0 :
+    print("Number is negative")
